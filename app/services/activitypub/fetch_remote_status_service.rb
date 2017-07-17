c3bacd6ce640e7bcd78945664911b5fd549d7a14
Eugen Rochko <eugen@zeonfederated.com>
2017-07-17 21:48:05 +0200

    Implement ActivityPub::FetchRemoteStatusService

--- app/services/activitypub/fetch_remote_status_service.rb
+++ app/services/activitypub/fetch_remote_status_service.rb
@@ -1,11 +1,35 @@
 # frozen_string_literal: true
 
 class ActivityPub::FetchRemoteStatusService < BaseService
-  def call(uri)
-    response = Request.new(:get, uri).perform
+  include JsonLdHelper
 
-    return nil unless response.code.successful?
+  def call(uri)
+    @json = fetch_resource(uri)
+
+    return unless supported_context? && expected_type?
+
+    attributed_to = first_of_value(@json['attributedTo'])
+    attributed_to = attributed_to['id'] if attributed_to.is_a?(Hash)
+
+    return unless trustworthy_attribution?(uri, attributed_to)
+
+    actor = ActivityPub::TagManager.instance.uri_to_resource(attributed_to, Account)
+    actor = ActivityPub::FetchRemoteAccountService.new.call(attributed_to) if actor.nil?
 
     raise NotImplementedError
+  end
+
+  private
+
+  def trustworthy_attribution?(uri, attributed_to)
+    Addressable::URI.parse(uri).normalized_host.casecmp(Addressable::URI.parse(attributed_to).normalized_host).zero?
+  end
+
+  def supported_context?
+    super(@json)
+  end
+
+  def expected_type?
+    %w(Note Article).include? @json['type']
   end
 end
